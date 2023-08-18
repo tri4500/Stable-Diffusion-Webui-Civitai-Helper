8e4aec433963cc0cef9f50a6a5166f8cad45a771
tri4500 <hodaitribm224@gmail.com>
2023-08-18 15:37:35 +0700

    add model info api

--- scripts/api.py
+++ scripts/api.py
@@ -27,10 +27,28 @@ def civitaiAPI(_: gr.Blocks, app: FastAPI):
         }
 
     @app.post('/civitai/v1/scan-model')
-    def link_status(scan_model_types: list = Body([],title="List of model types"),
+    def link_status(scan_model_types: list = Body(["ti", "hyper", "ckp", "lora"],title="List of model types"),
               max_size_preview: bool= Body(True,title="Download Max Size Preview"),
               skip_nsfw_preview: bool =Body(False,title="Skip NSFW Preview Images")):
         result = model_action_civitai.scan_model(scan_model_types, max_size_preview, skip_nsfw_preview)
         return { "result": result }
+    
+    @app.get('/civitai/v1/info-by-url')
+    def link_status(url: str = None):
+        r = model_action_civitai.get_model_info_by_url(url)
+        model_info = {}
+        model_name = ""
+        model_type = ""
+        subfolders = []
+        version_strs = []
+        if r:
+            model_info, model_name, model_type, subfolders, version_strs = r
+        return {
+           "model_info": model_info,
+           "model_name": model_name,
+           "model_type": model_type,
+           "subfolders": subfolders,
+           "versions": version_strs
+        }
 
 script_callbacks.on_app_started(civitaiAPI)
